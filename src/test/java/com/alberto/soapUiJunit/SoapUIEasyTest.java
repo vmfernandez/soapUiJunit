1ce99aa8e8b42fe1e876d6af93f467c84939077e
4lberto <4lberto@gmail.com>
2016-05-07 19:55:20 +0200

    Updated the names for both the test and the eclipse project

--- src/test/java/com/alberto/soapUiJunit/SoapUIEasyTest.java
+++ src/test/java/com/alberto/soapUiJunit/SoapUIEasyTest.java
@@ -24,14 +24,14 @@ public class SoapUIEasyTest {
 	public void shouldExecuteSpecificTestCase() throws XmlException, IOException, SoapUIException {
 		// given
 		WsdlProject project = new WsdlProject(TEST_FILE);
-		TestSuite testSuite = project.getTestSuiteByName("testSuite");
+		TestSuite testSuite = project.getTestSuiteByName("TestSuite");
 		TestCase testCase = testSuite.getTestCaseByName("TestCase");
 
 		for (int i = 0; i < 10; i++) {
 
 			PropertiesMap propertiesMap = new PropertiesMap();
 			//Sets parameter defined in Soap UI teste case
-			testCase.setPropertyValue("nombre", "parameter" + i);
+			testCase.setPropertyValue("name", "aName_" + i);
 
 			TestRunner runner = testCase.run(propertiesMap, false);
 			assertEquals(Status.FINISHED, runner.getStatus());
